30bf65e84e1964835d7cebacfe62eeee225c017b
Javier Sanz Sáez <javiersse@gmail.com>
2025-06-06 22:05:51 +0200

    feat: add ChainSelector component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
-import { Button, Card, Center, Footer, Header, Navbar, Page } from 'decentraland-ui'
-import { Props } from './App.types'
-import './App.css'
+import React from "react";
+import {
+  Button,
+  Card,
+  Center,
+  Footer,
+  Header,
+  HeaderMenu,
+  Navbar,
+  Page,
+} from "decentraland-ui";
+import { Props } from "./App.types";
+import "./App.css";
+import { ChainSelector } from "../ChainSelector/ChainSelector";
+import { ChainId } from "@dcl/schemas";
 
-const App: React.FC<Props> = ({ address, isConnected, onConnect, isConnecting, error }) => {
+const App: React.FC<Props> = ({
+  address,
+  isConnected,
+  onConnect,
+  isConnecting,
+  error,
+}) => {
   return (
     <>
       <Navbar activePage="Wallet" />
@@ -18,10 +35,26 @@ const App: React.FC<Props> = ({ address, isConnected, onConnect, isConnecting, e
             </>
           ) : (
             <Card>
-              <Header>Wallet</Header>
+              <HeaderMenu>
+                <HeaderMenu.Left>
+                  <Header as="h2">Wallet</Header>
+                </HeaderMenu.Left>
+                <HeaderMenu.Right>
+                  <ChainSelector
+                    chains={[ChainId.ETHEREUM_SEPOLIA, ChainId.MATIC_AMOY]}
+                    onSelectChain={(chain) => console.log(chain)}
+                    selectedChain={ChainId.ETHEREUM_SEPOLIA}
+                    i18n={{
+                      title: "Select Network",
+                      connected: "Connected",
+                      confirmInWallet: "Confirm in wallet",
+                    }}
+                  />
+                </HeaderMenu.Right>
+              </HeaderMenu>
               <p>
                 <strong>Address:</strong>&nbsp;
-                {address.slice(0, 6) + '...' + address.slice(-4)}
+                {address.slice(0, 6) + "..." + address.slice(-4)}
               </p>
             </Card>
           )}
@@ -29,7 +62,7 @@ const App: React.FC<Props> = ({ address, isConnected, onConnect, isConnecting, e
       </Page>
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/utils/media.ts
@@ -0,0 +1,89 @@
+import { useMediaQuery } from 'react-responsive'
+import React from 'react'
+
+/**
+ * Media hook to determine if we're going to be rendering in a desktop environment with big screens.
+ */
+const useBigDesktopMediaQuery = (): boolean => useMediaQuery({ minWidth: 1920 })
+
+/**
+ * Media hook to determine if we're going to be rendering in a desktop environment.
+ */
+const useDesktopMediaQuery = (): boolean => useMediaQuery({ minWidth: 992 })
+
+/**
+ * Media hook to determine if we're going to be rendering in a tablet environment.
+ */
+const useTabletMediaQuery = (): boolean =>
+  useMediaQuery({ minWidth: 768, maxWidth: 991 })
+
+/**
+ * Media hook to determine if we're going to be rendering in a tablet or below environment.
+ */
+const useTabletAndBelowMediaQuery = (): boolean =>
+  useMediaQuery({ maxWidth: 991 })
+
+/**
+ * Media hook to determine if we're going to be rendering in a mobile environment.
+ */
+const useMobileMediaQuery = (): boolean => useMediaQuery({ maxWidth: 767 })
+
+/**
+ * Media hook to determine if we're going to be rendering in an environment that's not mobile.
+ */
+const useNotMobileMediaQuery = (): boolean => useMediaQuery({ minWidth: 768 })
+
+/**
+ * Renders a component if the screen suits the desktop size.
+ */
+
+const Desktop = ({ children }: { children: React.ReactNode }) => {
+  const isDesktop = useDesktopMediaQuery()
+  return isDesktop ? children : null
+}
+
+/**
+ * Renders a component if the screen suits the tablet size.
+ */
+const Tablet = ({ children }: { children: React.ReactNode }) => {
+  const isTablet = useTabletMediaQuery()
+  return isTablet ? children : null
+}
+
+/**
+ * Renders a component if the screen suits the tablet or mobile size.
+ */
+const TabletAndBelow = ({ children }: { children: React.ReactNode }) => {
+  const isTablet = useTabletAndBelowMediaQuery()
+  return isTablet ? children : null
+}
+
+/**
+ * Renders a component if the screen suits the mobile size.
+ */
+const Mobile = ({ children }: { children: React.ReactNode }) => {
+  const isMobile = useMobileMediaQuery()
+  return isMobile ? children : null
+}
+
+/**
+ * Renders a component if the screen doesn't have the size of a mobile device.
+ */
+const NotMobile = ({ children }: { children: React.ReactNode }) => {
+  const isNotMobile = useNotMobileMediaQuery()
+  return isNotMobile ? children : null
+}
+
+export {
+  Desktop,
+  Tablet,
+  TabletAndBelow,
+  Mobile,
+  NotMobile,
+  useBigDesktopMediaQuery,
+  useDesktopMediaQuery,
+  useTabletMediaQuery,
+  useTabletAndBelowMediaQuery,
+  useMobileMediaQuery,
+  useNotMobileMediaQuery
+}
